f64aa907641cf530c545910bd612e8fd7f74a601
Yogurt7v <egorov-2k@yandex.ru>
2026-03-25 20:46:02 +0300

    fix: использовать NEXT_PUBLIC_POCKETBASE_URL на клиенте

--- src/shared/lib/pocketbase.ts
+++ src/shared/lib/pocketbase.ts
@@ -2,7 +2,10 @@ import PocketBase from 'pocketbase';
 
 const getPocketBaseUrl = () => {
   if (typeof window !== 'undefined') {
-    return window.location.origin.replace(':3000', ':8090');
+    return (
+      process.env.NEXT_PUBLIC_POCKETBASE_URL ||
+      window.location.origin.replace(':3000', ':8090')
+    );
   }
   return process.env.POCKETBASE_URL || 'http://127.0.0.1:8090';
 };
